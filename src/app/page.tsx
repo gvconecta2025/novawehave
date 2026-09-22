1f31430894aea9f65a0a6760c75fd31a1201aced
gvconecta2025 <gvconecta2025@gmail.com>
2026-09-22 14:47:03 -0300

    Delete src/app/page.tsx

--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function RotaRaiz() {
-  // Captura o usuário que acessa o link principal e envia para a segurança do Login
-  redirect('/login');
-}
